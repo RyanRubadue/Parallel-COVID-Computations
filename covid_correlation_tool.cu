4b46e4e6ba09ef5b552d69c2170fb7d43b2dc5f5
RyanRubadue <55749917+RyanRubadue@users.noreply.github.com>
2021-12-01 19:36:23 -0500

    add results portion

--- covid_correlation_tool.cu
+++ covid_correlation_tool.cu
@@ -41,6 +41,8 @@ const int COLUMN_NUMBER_3 = 31; // Column #
 const int COLUMN_NUMBER_4 = 35; // Column #
 const int NUM_RECORDS = 130600; // Number of Records to read
 const int NUM_CLUSTERS = 5;
+const int NUM_PAIRS = 6; //Number of possible Column pairings
+const int NUM_COLUMNS = 4; //Number of columns we are using
 
 void build_input(float input_1[], float input_2[], float input_3[], float input_4[], int locations[]){
 	ifstream file;
@@ -211,6 +213,145 @@ __global__ void classify(int clusters[], int locations[], int mapping[], int* ma
 			}
 		}
 	}
+}
+
+
+__global__ void calculate_correlations(float result_data[], float correlations[]) {
+    int xIndex = 0;
+    int yIndex = 0;
+    int increment = NUM_COLUMNS -1;
+    int currIndex = threadIdx.x;
+
+    //calculate the two indices of the data we are comparing using current thread
+    while(currIndex >= (NUM_COLUMNS -1)){
+	xIndex++;
+        increment--;
+	if(increment < 1){
+	     printf("Error calculating current Indexes to calculate correlations\n");
+	     return;
+	}
+        currIndex -= increment;
+    }
+    xIndex *= NUM_RECORDS;
+    yIndex = (1 + currIndex) * NUM_RECORDS;
+    //printf("hello from thread %d. I have xIndex %d and yIndex %d. Current increment is %d. Curr index %d\n", threadIdx.x, xIndex, yIndex, increment, currIndex);
+
+    if(xIndex < 0 || xIndex >= NUM_COLUMNS * NUM_RECORDS || yIndex < 0 || yIndex >= NUM_COLUMNS * NUM_RECORDS || xIndex == yIndex){
+	printf("Invalid indices calculated during correlation calculation function\n");
+	return;
+    }
+
+    __syncthreads();
+    if(threadIdx.x < NUM_PAIRS){
+	    // Calculate mean of each dataset
+	    float meanx = 0;
+	    float meany = 0;
+	    for (int i = 0; i < NUM_RECORDS; i++) {
+		meanx = meanx + 0.0001 * result_data[xIndex + i];
+		meany = meany + 0.0001 * result_data[yIndex + i];
+	    }
+	    meanx = meanx / (NUM_RECORDS * 0.0001);
+	    meany = meany / (NUM_RECORDS * 0.0001);
+	    // Calculate deviation scores and product of deviation scores
+	    float ssx = 0;
+	    float ssy = 0;
+	    float xy = 0;
+	    for (int i = 0; i < NUM_RECORDS; i++) {
+		ssx = ssx + 0.0001 * pow(result_data[xIndex + i] - meanx, 2);
+		ssy = ssy + 0.0001 * pow(result_data[yIndex + i] - meany, 2);
+		xy = xy + 0.0001 * (result_data[xIndex + i] - meanx) * (result_data[yIndex + i] - meany);
+	    }
+
+	    // Calculate correlation
+	    correlations[threadIdx.x] = (xy / sqrt(ssx * ssy));
+	    __syncthreads();
+    }
+    else printf("Invalid thread number\n");
+}
+
+
+__global__ void display_correlations(float correlations[]){
+	__syncthreads();
+	//float correlation = correlations[threadIdx.x];
+	int xIndex = 0;
+	int yIndex = 0;
+	int increment = NUM_COLUMNS -1;
+	int currIndex = threadIdx.x;
+	
+	while(currIndex >= NUM_COLUMNS -1){
+	    increment--;
+	    xIndex++;
+	    if(increment < 1){
+	         printf("Error calculating current Indexes to display correlations\n");
+	         return;
+	    }
+        currIndex -= increment;
+        }
+        xIndex += 1;
+        yIndex = (2 + currIndex);
+
+	if (abs(correlations[threadIdx.x]) > 1) {
+		printf("Invalid correlation value. Exiting\n");
+		return;
+	}
+	//__syncthreads();
+	if(abs(correlations[threadIdx.x]) > 0.7){
+		if(correlations[threadIdx.x] > 0) printf("Columns %d and %d have a strong positive correlation of %f\n", xIndex, yIndex, correlations[threadIdx.x]);
+		else printf("Columns %d and %d have a strong negative correlation of %f\n", xIndex, yIndex, correlations[threadIdx.x]);
+	}
+	else if(abs(correlations[threadIdx.x]) > 0.5){
+		if(correlations[threadIdx.x] > 0) printf("Columns %d and %d have a moderate positive correlation of %f\n", xIndex, yIndex, correlations[threadIdx.x]);
+		else printf("Columns %d and %d have a moderate negative correlation of %f\n", xIndex, yIndex, correlations[threadIdx.x]);
+	}
+	else if(abs(correlations[threadIdx.x]) > 0.3){
+		if(correlations[threadIdx.x] > 0) printf("Columns %d and %d have a weak positive correlation of %f\n", xIndex, yIndex, correlations[threadIdx.x]);
+		else printf("Columns %d and %d have a weak negative correlation of %f\n", xIndex, yIndex, correlations[threadIdx.x]);
+	}
+	else printf("Columns %d and %d have little-to-no correlation of %f\n", xIndex, yIndex, correlations[threadIdx.x]);
+	return;
+}
+
+
+__global__ void calculate_linear_regressions(float correlations[], float data[])
+{
+    float sumx = 0;
+    float sumy = 0;
+    float sumxy = 0;
+    float sumxSquared = 0;
+    float sumySquared = 0;
+    int xIndex = 0;
+    int yIndex = 0;
+    int increment = NUM_COLUMNS -1;
+    int currIndex = threadIdx.x;
+
+    //calculate the two indices of the data we are comparing using current thread
+    while(currIndex >= (NUM_COLUMNS -1)){
+	xIndex++;
+        increment--;
+	if(increment < 1){
+	     printf("Error calculating current Indexes to calculate linear regressions\n");
+	     return;
+	}
+        currIndex -= increment;
+    }
+    xIndex *= NUM_RECORDS;
+    yIndex = (1 + currIndex) * NUM_RECORDS;
+    
+    if(abs(correlations[threadIdx.x]) < 0.3){
+	printf("Minimal correlation between quanitifiers %d and %d. Skipping Calculating Regression.\n", xIndex / NUM_RECORDS + 1, yIndex / NUM_RECORDS + 1);
+	return;
+    }
+
+    for (int i = 0; i < NUM_RECORDS; i++) {
+        sumx = sumx + 0.0001 * data[xIndex + i];
+        sumy = sumy + 0.0001 * data[yIndex + i];
+        sumxy = sumxy + 0.0001 * (data[xIndex + i] * data[yIndex + i]);
+        sumxSquared = sumxSquared + 0.0001 * pow(data[xIndex + i], 2);
+        sumySquared = sumySquared + 0.0001 * pow(data[yIndex + i], 2);
+    }
+    float a = 10000 * (((NUM_RECORDS * sumxy) - (sumx * sumy)) / ((NUM_RECORDS * sumxSquared) - pow(sumx, 2)));
+    float b = 10000 * (((sumy * sumxSquared) - (sumx * sumxy)) / ((NUM_RECORDS * sumxSquared) - pow(sumx, 2)));
+    printf("The calculated linear regression for columns %d and %d is %fx + %f\n", xIndex / NUM_RECORDS + 1, yIndex / NUM_RECORDS + 1, a, b);
 }	
 
 int main() {
@@ -330,13 +471,52 @@ int main() {
 	HANDLE_ERROR( cudaEventElapsedTime( &elapsedTime, start, stop ) );
 	printf( "Time to Analyze:  %3.1f ms\n", elapsedTime );
 	HANDLE_ERROR( cudaEventRecord( start, 0 ) );
+	
+	//Results - Ryan
 
-	//Ryan
+	cout << "Beginning Display of Results\n\n";
+
+	// Allocate device memory
+        float *dev_correlations;
+        float *dev_result_data;
+
+	float result_data[SIZE_F * NUM_COLUMNS];
+	float *correlations = new float[NUM_PAIRS];
+
+        for(int i =0; i < NUM_PAIRS; i++) correlations[i] = 0;
+	for(int i = 0; i < NUM_RECORDS; i++) result_data[i] = input_1[i];
+	for(int i = 0; i < NUM_RECORDS; i++) result_data[i + NUM_RECORDS] = input_2[i];
+	for(int i = 0; i < NUM_RECORDS; i++) result_data[i + 2*NUM_RECORDS] = input_3[i];
+	for(int i = 0; i < NUM_RECORDS; i++) result_data[i+3*NUM_RECORDS] = input_4[i];
+
+	HANDLE_ERROR( cudaMalloc( (void**)&dev_result_data, SIZE_F * NUM_COLUMNS ) );
+	HANDLE_ERROR( cudaMemcpy( dev_result_data, result_data, SIZE_F * NUM_COLUMNS, cudaMemcpyHostToDevice) );  
+
+	HANDLE_ERROR( cudaMalloc( (void**)&dev_correlations, NUM_PAIRS * sizeof(float) ) );
+	HANDLE_ERROR( cudaMemcpy( dev_correlations, correlations, NUM_PAIRS * sizeof(float), cudaMemcpyHostToDevice) );  
+
+	cout << "Calculating Correlations... ";
+	calculate_correlations<<<1, NUM_PAIRS>>>(dev_result_data, dev_correlations);
+	cudaDeviceSynchronize();
+
+	HANDLE_ERROR( cudaMemcpy(correlations, dev_correlations, NUM_PAIRS * sizeof(float), cudaMemcpyDeviceToHost));
+	cout << "\n\nDisplaying Calculated Correlations: \n\n";
+
+	// Output Correlation Strengths
+
+	display_correlations<<<1, NUM_PAIRS>>>(dev_correlations);
+	cudaDeviceSynchronize();
+
+	//Calculate Linear Regressions
+	cout << "\nCalulating Linear Regressions\n\n";
+	calculate_linear_regressions<<<1, NUM_PAIRS>>>(dev_correlations, dev_result_data);
+	cudaDeviceSynchronize();
+
 
 	HANDLE_ERROR( cudaEventRecord( stop, 0 ) );
 	HANDLE_ERROR( cudaEventSynchronize( stop ) );
 	HANDLE_ERROR( cudaEventElapsedTime( &elapsedTime, start, stop ) );
-	printf( "Time to Output:  %3.1f ms\n", elapsedTime );
+	printf( "\nTime to Output:  %3.1f ms\n", elapsedTime );
 
 	HANDLE_ERROR( cudaEventDestroy( start ) );
 	HANDLE_ERROR( cudaEventDestroy( stop ) );
@@ -345,6 +525,8 @@ int main() {
 	cudaFree( dev_clusters );
 	cudaFree( dev_change_clusters );
 	cudaFree( dev_max_loc );
+	cudaFree ( dev_correlations);
+	cudaFree (dev_data);
 	
 	return 0;
 }
